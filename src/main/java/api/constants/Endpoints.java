4cd57ef0e04f325101da488eb977874e0e464221
Johnta19 <m.eigster20@gmail.com>
2021-08-27 01:37:27 +0300

    ignore allure-results

--- src/main/java/api/constants/Endpoints.java
+++ src/main/java/api/constants/Endpoints.java
@@ -4,4 +4,5 @@ public class Endpoints {
     public static final String BASE_URL = "https://restful-booker.herokuapp.com";
     public static final String POST_REQUEST = "/auth";
     public static final String PATH_TO_BOOKING = "/booking/";
+
 }
